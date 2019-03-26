be5a7b32afa121c468f6fdf4f5e359b7e03c452d
livesxu <livesxu@163.com>
2019-03-26 20:51:53 +0800

    装饰，内边距，外边距，边框，圆角，阴影，背景图，混合模式

--- test_app/lib/Home/BasicTextDemo.dart
+++ test_app/lib/Home/BasicTextDemo.dart
@@ -22,13 +22,60 @@ class BasicTextDemo extends StatelessWidget {
   }
 
   @override
-
+//富文本
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return RichText(text: TextSpan(text: "Hello,Livesxu\n",style: TextStyle(fontSize: 20,color: Colors.red,),children: [
+//      TextSpan(text: "my name is Livesxu",style: TextStyle(fontSize: 10,color: Colors.green))
+//    ]));
+//  }
   Widget build(BuildContext context) {
     // TODO: implement build
-    return RichText(text: TextSpan(text: "Hello,Livesxu\n",style: TextStyle(fontSize: 20,color: Colors.red,),children: [
-      TextSpan(text: "my name is Livesxu",style: TextStyle(fontSize: 10,color: Colors.green))
-    ]));
+    return Container(
+      
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage("https://b-ssl.duitang.com/uploads/item/201812/11/20181211022124_kurof.jpg"),
+          alignment: Alignment.bottomCenter,
+          repeat: ImageRepeat.repeatY,
+//          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(Colors.blue.withOpacity(.4), BlendMode.hardLight)
+        ),
+
+      ),
+//      color: Colors.grey[100],
+      child: Row(
+//        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Icon(Icons.call,color: Colors.white,),
+//            color: Color.fromRGBO(3, 53, 255, 1),
+            padding: EdgeInsets.all(10),//内边距
+            margin: EdgeInsets.all(50),//外边距
+            width: 90,
+            height: 90,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 53, 255, 1),
+//              border: Border(top: BorderSide(color: Colors.greenAccent,width: 5)),//单边框
+              border: Border.all(color: Colors.greenAccent,width: 5),//统一边框
+//              borderRadius: BorderRadius.only(bottomRight: Radius.circular(10)),//单圆角
+//              borderRadius: BorderRadius.circular(20),//统一圆角
+              boxShadow: [
+                BoxShadow(color: Colors.green,offset: Offset(5, 10),blurRadius: 10,spreadRadius: 10)
+              ],
+              shape: BoxShape.circle,//形状，圆形的盒子上面不能使用圆角属性！
+//              gradient:LinearGradient(colors: [Colors.red,Colors.blue],begin: Alignment.topCenter,end: Alignment.bottomCenter),//线性渐变
+              gradient: RadialGradient(colors: [Colors.red,Colors.blue]),//星型渐变，辐射渐变
+            ),
+            
+          ),
+        ],
+      ),
+    );
   }
 }
 //rich  adj.丰富的
 //span  n.范围
+//blur  v.模糊
+//spread v.展开
+//gradient 斜坡 - 渐变
