d9118372cf52c24aa6bfe64dc97ac8566c5daab2
livesxu <livesxu@163.com>
2019-04-09 15:47:14 +0800

    创建新演示，初始路由进入checkBox页面

---
+++ test_app/lib/Mine/CheckBoxDemo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CheckBoxDemo extends StatefulWidget {
+
+  @override
+
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return CheckBoxDemoState();
+  }
+}
+
+class CheckBoxDemoState extends State<CheckBoxDemo> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:test_app/Home/HomeViewController.dart';
 import 'package:test_app/Home/HomeSubViewController.dart';
 import 'package:test_app/Mine/MineViewController.dart';
 import 'package:test_app/Mine/ComponentsDemo.dart';
+import 'package:test_app/Mine/CheckBoxDemo.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,12 +26,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
 //      home: HomeViewController(),
-      initialRoute: "/components",//初始页面
+      initialRoute: "/checkBox",//初始页面
       routes: {
         "/":(context) => HomeViewController(),//路由映射
         "/routerNameToNext":(context) => HomeSubHomeViewController(title: "RouterNameToNext",),
         "/mine":(context) => MineViewController(),
         "/components":(context) => ComponentsDemo(),
+        "/checkBox":(context) => CheckBoxDemo(),
       },
     );
   }
