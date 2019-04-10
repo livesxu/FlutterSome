0521589e8bcc4b003a541b0d24e70048b7336464
livesxu <livesxu@163.com>
2019-04-10 19:58:18 +0800

    Radio 单选按钮，互斥单选

--- test_app/lib/Mine/CheckBoxDemo.dart
+++ test_app/lib/Mine/CheckBoxDemo.dart
@@ -15,10 +15,54 @@ class CheckBoxDemoState extends State<CheckBoxDemo> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return CheckDemoExample();
+    return RadioDemoExample();
   }
 }
 
+class RadioDemoExample extends StatefulWidget {
+
+  @override
+
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return RadioDemoExampleState();
+  }
+}
+
+//单选框 - 互斥
+class RadioDemoExampleState extends State<RadioDemoExample> {
+
+  int groupValue;//选定的那个值，当与value值相同时选中效果
+
+  void _changeAction (value){
+
+    setState(() {
+      groupValue = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("RadioDemo"),),
+      body: Column(
+        children: <Widget>[
+//          Radio(value: 1, groupValue: groupValue, onChanged:_changeAction),
+//          Radio(value: 2, groupValue: groupValue, onChanged:_changeAction),
+//          Radio(value: 3, groupValue: groupValue, onChanged:_changeAction),
+
+          RadioListTile(value: 1, groupValue: groupValue, onChanged: _changeAction,title: Text("1"),subtitle: Text("01"),selected: groupValue==1,),
+          RadioListTile(value: 2, groupValue: groupValue, onChanged: _changeAction,title: Text("2"),subtitle: Text("02"),),
+          RadioListTile(value: 3, groupValue: groupValue, onChanged: _changeAction,title: Text("3"),subtitle: Text("03"),),
+        ],
+      )
+    );
+  }
+}
+
+
+//复选框
 class CheckDemoExample extends StatefulWidget {
 
   @override
