86a9e5fa968e8dcaea39401e480ec54d415a9a65
livesxu <livesxu@163.com>
2019-04-10 19:34:54 +0800

    CheckBox复选框

--- test_app/lib/Mine/CheckBoxDemo.dart
+++ test_app/lib/Mine/CheckBoxDemo.dart
@@ -15,6 +15,40 @@ class CheckBoxDemoState extends State<CheckBoxDemo> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return CheckDemoExample();
+  }
+}
+
+class CheckDemoExample extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return CheckDemoExampleState();
+  }
+}
+
+class CheckDemoExampleState extends State<CheckDemoExample> {
+
+  bool ischeck = false;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("checkBoxDemo"),),
+      body: Checkbox(
+          value: ischeck,
+          checkColor: Colors.blue,
+          activeColor: Colors.red,
+          onChanged: (value){
+
+            setState(() {
+
+              ischeck = value;
+            });
+          }
+      ),
+    );;
   }
 }
