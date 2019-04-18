986cc235244f469c53a6d33ec06261990dfd7cf9
livesxu <livesxu@163.com>
2019-04-18 19:53:52 +0800

    传值利用InheritedWidget共享子树数据使用

--- test_app/lib/Mine/StateDemo.dart
+++ test_app/lib/Mine/StateDemo.dart
@@ -20,33 +20,86 @@ class StateDemoState extends State<StateDemo> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: Text("StateDemo"),),
-      body: Center(
-        child: CustomChipItem(_count, _pressedAction),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _pressedAction,
-        child: Icon(Icons.add),
+    return InheritedProvider(
+      count: _count,
+      callback: _pressedAction,
+      child: Scaffold(
+        appBar: AppBar(title: Text("StateDemo"),),
+        body: Center(
+          child: CustomChipItemWrapper(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _pressedAction,
+          child: Icon(Icons.add),
+        ),
       ),
     );
+  }
+}
+
+/// ```dart - 系统示例
+/// class FrogColor extends InheritedWidget {
+///   const FrogColor({
+///     Key key,
+///     @required this.color,
+///     @required Widget child,
+///   }) : assert(color != null),
+///        assert(child != null),
+///        super(key: key, child: child);
+///
+///   final Color color;
+///
+///   static FrogColor of(BuildContext context) {
+///     return context.inheritFromWidgetOfExactType(FrogColor) as FrogColor;
+///   }
+///
+///   @override
+///   bool updateShouldNotify(FrogColor old) => color != old.color;
+/// }
+
+class InheritedProvider extends InheritedWidget {
+
+  final int count;
+  final VoidCallback callback;
+  final Widget child;
+
+  InheritedProvider({
+
+    this.count,
+    this.callback,
+    this.child,
+  }) : super(child:child);
+
+  static InheritedProvider of(BuildContext context) {
+    
+    return context.inheritFromWidgetOfExactType(InheritedProvider);
+  }
+  
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    return true;
   }
 }
 
 class CustomChipItem extends StatelessWidget {
 
-  int _count;
-
-  VoidCallback _callback;
-
-  CustomChipItem(this._count,this._callback);
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return ActionChip(
-      label: Text("$_count"),
-      onPressed: _callback,
+      label: Text("${InheritedProvider.of(context).count}"),
+      onPressed: InheritedProvider.of(context).callback,
     );
   }
 }
+
+class CustomChipItemWrapper extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return CustomChipItem();
+  }
+}
+
