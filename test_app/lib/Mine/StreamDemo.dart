843535a259c81f2d702bb0ed30cf58e767476285
livesxu <livesxu@163.com>
2019-04-23 17:14:13 +0800

    StreamController控制Stream

--- test_app/lib/Mine/StreamDemo.dart
+++ test_app/lib/Mine/StreamDemo.dart
@@ -37,15 +37,21 @@ class StreamDemoExampleState extends State<StreamDemoExample> {
   StreamController<String> _streamController;
 
   @override
+  void dispose() {
+
+    _streamController.close();//关闭
+    super.dispose();
+  }
+
+  @override
   void initState() {
     super.initState();
     
-    Stream<String> _streamDemo = Stream.fromFuture(delayData());
+//    Stream<String> _streamDemo = Stream.fromFuture(delayData());
 
-//    _streamController = StreamController<String>();
+    _streamController = StreamController<String>();
 
-    _streamSubscripthon = _streamDemo.listen(onDataAction,onError: onErrorAction,onDone: onDoneAction,cancelOnError: false,);
-
+    _streamSubscripthon = _streamController.stream.listen(onDataAction,onError: onErrorAction,onDone: onDoneAction,cancelOnError: false,);
   }
   
   Future<String> delayData () async {
@@ -55,6 +61,14 @@ class StreamDemoExampleState extends State<StreamDemoExample> {
 //    throw "something happened";
 
     return "Hello World ~";
+  }
+
+  void addData () async {
+
+    String data = await delayData();
+
+    _streamController.add(data);
+
   }
 
   void onDataAction (String string){
@@ -79,6 +93,7 @@ class StreamDemoExampleState extends State<StreamDemoExample> {
     // TODO: implement build
     return Center(
       child: Row(mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[
+        RaisedButton(onPressed: (){addData();}, child: Text("add")),//添加
         RaisedButton(onPressed: (){_streamSubscripthon.pause();print("pause");}, child: Text("pause")),//暂停
         RaisedButton(onPressed: (){_streamSubscripthon.resume();print("resume");}, child: Text("resume")),//恢复
         RaisedButton(onPressed: (){_streamSubscripthon.cancel();print("cancel");}, child: Text("cancel")),//取消
