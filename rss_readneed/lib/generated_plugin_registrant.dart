8358cb452f781e17d84683d68e0e5683880da7fc
Livesxu <livesxu@163.com>
2020-07-28 22:23:46 +0800

    添加定位，未执行。。android的链接唤起未执行，剪切板未验证

--- rss_readneed/lib/generated_plugin_registrant.dart
+++ rss_readneed/lib/generated_plugin_registrant.dart
@@ -3,11 +3,13 @@
 //
 import 'dart:ui';
 
+import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins(PluginRegistry registry) {
+  LocationWebPlugin.registerWith(registry.registrarFor(LocationWebPlugin));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   registry.registerMessageHandler();
 }
